fd577a88e301c97f109dae0d2686c4df582754db
Dai Kawahara <skbsporta7@gmail.com>
2020-05-21 00:19:46 +0900

    add presenter

---
+++ Pinterest-Layout-Sample/Presenter/PhotoListViewPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoListViewPresenter.swift
+//  Pinterest-Layout-Sample
+//
+//  Created by kawaharadai on 2020/05/20.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import Foundation
+
+protocol PhotoListViewPresenter {}
+
+final class PhotoListViewPresenterImpl: PhotoListViewPresenter {}
